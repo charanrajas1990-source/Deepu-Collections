c93a6fd46b24f3610e700e9364da78f380176611
Charansingh <charansingh@Charansinghs-MacBook-Air-6.local>
2026-09-12 20:54:07 +0530

    Fix Firebase config for Vercel build

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANG0jhgKC9DX5YTZ3fu6bDHOkFadyTyn8",
+  authDomain: "deepu-collections.firebaseapp.com",
+  projectId: "deepu-collections",
+  storageBucket: "deepu-collections.firebasestorage.app",
+  messagingSenderId: "893628997673",
+  appId: "1:893628997673:web:1e8ccf947547f069f43c38"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId); 
-export const auth = getAuth();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
