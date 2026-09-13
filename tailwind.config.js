226970e6e1d73188fd2e0e7abaa94454c064e2e8
Charansingh <charansingh@Charansinghs-MacBook-Air-6.local>
2026-09-13 17:40:38 +0530

    style: revert background theme back to bright cream (#FCF6E8)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'theme-cream': '#F7F1F8',
+        'theme-cream': '#FCF6E8',
         'theme-maroon': '#3B1A4A',
         'theme-lining': '#5C167D'
       }
